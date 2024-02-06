3b4fc23b2abf4e1ad9c79eee624ad586c05524cb
Ramiro <carbajalramiro02@gmail.com>
2024-02-05 21:00:48 -0300

    Inform about the log in process

---
+++ README.txt
@@ -0,0 +1,9 @@
+Welcome to Ramiro Carbajal's project.
+
+My project involves a registration and login form.
+
+The registration form will ask for your first name, last name, email, and password.
+Once you complete it, you'll proceed to the login stage, where you'll need to enter your email and the previously set password.
+If you input the wrong email, the website will say 'User not found.'
+If you enter the wrong password, the website will inform you 'Passwords do not match.'
+And if you enter everything correctly, the website will say 'Login successful. Welcome, (your email).'
--- js/auth.js
+++ js/auth.js
@@ -43,24 +43,26 @@ const save_User = (user) => {
 }
 
 const loginUser = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
 
     //we loof for the email that our user wrote to see if it exist
-    const userFound = db_users.find((item) => item.email === login_email.value)
+    const userFound = db_users.find((item) => item.email === login_email.value);
+    const messageContainer = document.getElementById("messageContainer");
 
     // if the email exists...
     if (userFound) {
-        if (userFound.password === login_password.value) {   
-            console.log("You logged in successfully") 
-        } else {                                             //check if the passwords are the same   
-            console.log("Passwords do not match")
-        }   
-    } else { 
-        console.log("User not found")                   //if the email do not exist 
+        if (userFound.password === login_password.value) {
+            messageContainer.textContent = "Login successful. Welcome, " + userFound.email + "!";
+            messageContainer.className = "message-container success-message";
+        } else {                                                                           //check if the passwords are the same
+            messageContainer.textContent = "Passwords do not match";
+            messageContainer.className = "message-container error-message";
+        }
+    } else {
+        messageContainer.textContent = "User not found";                     //if the email do not exist 
+        messageContainer.className = "message-container error-message";
     }
-    
 }
-
 
 form_register.addEventListener("submit",createUser)     //when click the register button, execute the function "createUser"
 form_login.addEventListener("submit",loginUser)         //when click the Log in button, execute the function "loginUser"
